5f6f28276ae3ea60ab939662936c8a609f7168a7
muhammadtalhagit <muhammadtalha.aptech2006@gmail.com>
2024-11-24 19:40:57 +0500

    First Commit but incomplete assignment

---
+++ api-integration/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+
+export const routes: Routes = [
+
+    {
+        path:"",
+        component:EventsComponent
+        
+    }
+];
---
+++ api-integration/src/app/events/events.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  standalone: true,
+  imports: [],
+  templateUrl: './events.component.html',
+  styleUrl: './events.component.css'
+})
+export class EventsComponent {
+events:any=[];
+constructor(private http:HttpClient){
+this.getEvents();
+
+}
+
+getEvents(){
+  this.http.get("https://localhost:7065/api/EventOrganizer").subscribe((res:any)=>{
+this.events= res;
+console.log(res);
+
+  })
+}
+
+
+
+}
